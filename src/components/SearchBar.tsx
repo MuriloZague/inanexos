7d603357b8c192a657016a2611a316fd3f2132c1
Murilo <zaguemurilo7@gmail.com>
2025-12-15 14:20:49 -0300

    "horario-aps" form added + a new way to delete text from searchbar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -8,15 +8,16 @@ interface SearchBarProps {
 
 const SearchBar: React.FC<SearchBarProps> = ({ value, onChange }) => {
     return (
-        <div className="relative w-full max-w-2xl mx-auto">
-            <Search className="absolute left-3 top-[35%] -translate-y-1/2 h-5 w-5 text-muted-foreground" />
+        <div className="relative w-full max-w-2xl mx-auto flex items-center mb-4">
+            <Search className="absolute left-3 h-5 w-5"/>
             <input 
                 type="text" 
                 placeholder="Digite o nome do formulário"
                 value={value}
                 onChange={(e) => onChange(e.target.value)}
-                className="pl-10 w-full p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="pl-10 w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
+            <p className="cursor-pointer absolute right-3 text-[14px] font-stretch-200% bg-[#0a63d8] rounded-full p-2 pt-0.5 pb-0.5 text-white" onClick={() => onChange("")}>X</p>
         </div>
     );
 }
