a464b7f74a0dc4af4bd4dfa5a5d1e6ff1f2009a0
Murilo Zague <zaguemurilo7@gmail.com>
2026-02-05 09:32:04 -0300

    feat: adding cursor pointer to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,8 +7,8 @@ function Header() {
     
     return (
         <section className="bg-[#004db3] p-10 text-center">
-            <div className="text-white flex flex-col justify-center items-center">
-                <div className="flex text-4xl mb-2" onClick={() => navigate("/")}>
+            <div className="text-white flex flex-col justify-center items-center cursor-pointer" onClick={() => navigate("/")}>
+                <div className="flex text-4xl mb-2">
                     <FileSpreadsheet className="h-10 w-10 mr-3 hidden sm:block"/>
                     <p className="font-bold">Central de Formulários INSS</p>
                 </div>
