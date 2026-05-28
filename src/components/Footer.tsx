acc1ae76c1fb50ebe7e8d0cef72f2e82d82d5529
Murilo Zague <zaguemurilo7@gmail.com>
2026-05-28 08:54:35 -0300

    fix: corrigindo formulário antigo 2025

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,8 @@
 function Footer() {
     return ( 
         <>
-            <p className="text-center mt-10">Só imprima o necessário. Preserve o meio ambiente.</p>
+            <p className="text-center mt-10">Só imprima o necessário. Preserve o meio ambiente.<br/>
+           Caso tenha sugestões de formulários a serem adicionados, entre em contato pelo e-mail: murilo.zague@inss.gov.br</p>
         </>
      );
 }
