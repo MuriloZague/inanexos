a773f375f976fcfa885a7853b5b41f5c9a15c42c
Murilo Zague <zaguemurilo7@gmail.com>
2026-09-24 09:44:19 -0300

    chore: atualizando aviso no footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
     return ( 
         <>
             <p className="text-center mt-10">Só imprima o necessário. Preserve o meio ambiente.<br/>
-           Caso tenha sugestões de formulários a serem adicionados, entre em contato pelo e-mail: murilo.zague@inss.gov.br</p>
+           Caso tenha sugestões de formulários/documentos a serem adicionados, entre em contato pelo e-mail: murilo.zague@inss.gov.br</p>
         </>
      );
 }
